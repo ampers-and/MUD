4df6dbd6bfc928a4486fc75ecaa058dada61eec7
ampersand <hossainazifa@gmail.com>
2020-03-02 15:11:03 -0800

    testing with loop

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,36 +1,43 @@
 import React from "react";
-import { maze } from "../utils/main_maze";
+import { maze, loop } from "../utils/main_maze";
 
 const Map = () => {
   let grid = [];
 
-  for (let i = 0; i < 26; i++) {
+  for (let i = 0; i < 10; i++) {
     grid.push([]);
-    for (let j = 0; j < 30; j++) {
+    for (let j = 0; j < 10; j++) {
       grid[i].push({ room: "?", n: "?", s: "?", e: "?", w: "?" });
     }
   }
 
-  console.log("maze", maze);
+  console.log("maze", loop);
 
   console.log("grid", grid);
 
-  for (let room in maze) {
-    let r = maze[room];
+  for (let room in loop) {
+    let r = loop[room];
 
-    let x = r[0][0];
-    let y = r[0][1] - 2;
+    let x = 10 - r[0][1];
+    let y = r[0][0];
     grid[x][y] = { ...grid[x][y], room: room, ...r[1] };
   }
 
   console.log("grid", grid);
 
-  let nonStyle = {
-    borderColor: "dimgray"
-  };
-
-  let northStyle = {
-    borderColorTop: "lime"
+  let style = j => {
+    if (j.room === "?") {
+      return { borderColor: "dimgray" };
+    } else {
+      let n,
+        s,
+        w,
+        e = "white";
+      for (k in j) {
+        if (j[n] !== "?") {
+        }
+      }
+    }
   };
 
   return (
@@ -42,7 +49,7 @@ const Map = () => {
               key={Number(String(index) + String(jndex))}
               className={`column ${jndex}`}
               id={`box ${index} ${jndex}`}
-              style={j.room === "?" ? nonStyle : null}
+              style={style(j)}
             >
               {j.room !== "?" && <p>{j.room}</p>}
             </div>
